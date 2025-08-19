f4824d6d518bc223159f38b764336bd2e032c60e
selva <selva@DESKTOP-1R3F6R3>
2025-08-19 21:28:43 +0530

    commit

---
+++ CoreJavaProgram/src/Day2/Executor.java
@@ -0,0 +1,16 @@
+package Day2;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		Person p=new Person();
+		p.setId(101);
+		p.setName("Madesh");
+		p.setAge(20);
+		p.setCity("Pondy");
+		
+		System.out.println(p.toString());
+		
+	}
+
+}
